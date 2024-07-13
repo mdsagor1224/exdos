c4087f4926b6ef56c187e2ca0750b7f74d151e85
mdsagor <mdsagorahmed1224@gmail.com>
2024-07-13 20:57:34 +0600

    c28.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,27 @@
 (function($){
     "use strict";
 
+        // mobile menu 
+        var tpMenuWrap = $('.tp-mobile-menu-active > ul').clone();
+        var tpSideMenu = $('.tp-offcanvas-menu nav');
+        tpSideMenu.append(tpMenuWrap);
+        if ($(tpSideMenu).find('.sub-menu, .tp-mega-menu').length != 0) {
+          $(tpSideMenu).find('.sub-menu, .tp-mega-menu').parent().append('<button class="tp-menu-close"><i class="fas fa-chevron-right"></i></button>');
+        }
+    
+        var sideMenuList = $('.tp-offcanvas-menu nav > ul > li button.tp-menu-close, .tp-offcanvas-menu nav > ul li.has-dropdown > a');
+        $(sideMenuList).on('click', function (e) {
+          console.log(e);
+          e.preventDefault();
+          if (!($(this).parent().hasClass('active'))) {
+            $(this).parent().addClass('active');
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideDown();
+          } else {
+            $(this).siblings('.sub-menu, .tp-mega-menu').slideUp();
+            $(this).parent().removeClass('active');
+          }
+        });
+
 
     $(".tp-offcanvas-toogle").on('click', function(){
         $(".tp-offcanvas").addClass("tp-offcanvas-open");
